2584b44d3324ac9f0c04b099b5a285cdf1c061db
PerdomoV <jorgamow@gmail.com>
2021-07-04 14:38:30 -0500

    Now the environment variable process.env.PORT is used to serve the server :v

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require('express')
 
 
 const app = express()
-const port = 3000
 
+app.set('port', process.env.PORT || 3000)
 app.use(express.json())
 app.use('/api', taskRouter)
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(app.get('port'), () => {
+  console.log(`Example app listening at http://localhost:${app.get('port')}`)
   
 })
